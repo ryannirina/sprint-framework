d84f254f6db6610804c0005d50d84cdd5d92137d
ryannirina <ryannirina8@gmail.com>
2025-03-02 10:49:12 +0300

    Sprint 0

--- src/main/java/com/sprintframework/web/servlet/FrontController.java
+++ src/main/java/com/sprintframework/web/servlet/FrontController.java
@@ -16,15 +16,7 @@ public class FrontController extends HttpServlet {
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        // Get the request URI
         String uri = request.getRequestURI();
-        
-        // TODO: In future sprints, we'll implement:
-        // 1. Handler mapping
-        // 2. Controller execution
-        // 3. View resolution
-        
-        // For now, just echo back the requested URI
         response.setContentType("text/plain");
         response.getWriter().write("Sprint Framework - Requested URI: " + uri);
     }
